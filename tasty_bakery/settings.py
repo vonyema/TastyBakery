59c6f978c2afde618676c35403b1fdf9a210d3a1
Victoria Onyema <nolabby95@me.com>
2023-02-14 16:16:27 -0500

    heroku cofig

--- tasty_bakery/settings.py
+++ tasty_bakery/settings.py
@@ -27,7 +27,7 @@ MEDIA_URL = '/media/'
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
