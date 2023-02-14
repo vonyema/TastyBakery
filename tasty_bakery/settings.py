247661a8d0601a550f74efe9571cfeecd64b3875
Victoria Onyema <nolabby95@me.com>
2023-02-14 12:58:43 -0500

    heroku cofig

--- tasty_bakery/settings.py
+++ tasty_bakery/settings.py
@@ -131,6 +131,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_on_heroku
-django_on_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
 
