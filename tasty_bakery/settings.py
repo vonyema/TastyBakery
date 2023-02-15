88e63ddf69d48a6a1366ef96c1718ad79f6d922a
Victoria Onyema <nolabby95@me.com>
2023-02-15 18:28:16 -0500

    added login redirect url

--- tasty_bakery/settings.py
+++ tasty_bakery/settings.py
@@ -137,6 +137,9 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL='/'
+
 import django_heroku
 django_heroku.settings(locals())
 
